1c8dba30a6ecb768fa2773505a8aee4ca11bab3b
Raphael Smith <codesmithr@outlook.com>
2023-03-24 13:10:45 -0700

    add conditional to check invalid connection ready state

--- test/test.js
+++ test/test.js
@@ -13,6 +13,7 @@ const mongo_dev_uri = process.env.DEV_MONGODB;
 const mongo_invalid_uri = process.env.INVALID_DB;
 
 describe('Mongoose', () => {
+    mongoose.set('strictQuery', true);
     let conn_dev;
     let conn_test;
     let conn_invalid;
@@ -20,18 +21,23 @@ describe('Mongoose', () => {
     before("create connection", async () => {
         conn_dev  = await mongoose.createConnection(mongo_test_uri).asPromise();
         conn_test = await mongoose.createConnection(mongo_dev_uri).asPromise();
+        assert.rejects(async () => {
+            conn_invalid = await mongoose.createConnection(mongo_invalid_uri).asPromise();
+        }, (err) => err === 'Invalid scheme, expected connection string to start with "mongodb://" or "mongodb+srv://"');
 
         return;
     });
-    it("shouldnt connect to DB with incorrect prefix", async (done) => {
-        conn_invalid = mongoose.createConnection(mongo_invalid_uri);
-        const ready = conn_invalid.readyState;
-        
-        if (assert.equal(ready, 0) === 0) {
-            assert.rejects(await conn_invalid.asPromise());
-        };
-        done();
-        
+    it("shouldnt connect to DB with incorrect prefix", () => {
+        const options = {
+            autoIndex: false, // Don't build indexes
+            maxPoolSize: 10, // Maintain up to 10 socket connections
+            serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+            socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+            family: 4 // Use IPv4, skip trying IPv6
+          };
+        mongoose.connect(mongo_invalid_uri, options, function(error) {
+            if (error) return true;
+          });
     });
     it("should connect to Spec Test DB", async (done) => {
         const ready = conn_test.readyState;
